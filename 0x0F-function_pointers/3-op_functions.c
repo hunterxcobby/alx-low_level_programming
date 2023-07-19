d7a00f32d7e23f1dab74008d680d8d5d3180b8b3
hunterxcobby <solomonsefah13@gmail.com>
2023-07-19 23:46:11 +0000

    calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *get_op_func - entry point
+ *@s: operator passed
+ *Return: pointer to the function that corresponds to the
+ *operator given as a parameter
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int m = 0;
+
+	while (m < 5)
+	{
+		if (strcmp(s, ops[m].op) == 0)
+			return (ops[m].f);
+
+		m++;
+	}
+
+	return (0);
+}
