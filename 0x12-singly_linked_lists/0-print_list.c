8a4a9dc496ba9e6c573b45ab4c1fe18eced69558
hunterxcobby <solomonsefah13@gmail.com>
2023-07-27 19:40:02 +0000

    Added the first file from task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*print_list - a program that prints all elements of a list_t list
+*@h: name of list
+*Return: nodes' number
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t put;
+
+	put = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		put++;
+	}
+	return (put);
+}
