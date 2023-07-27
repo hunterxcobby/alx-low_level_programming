acc10523a685e8d68b428c6ca3135ae4814daa29
hunterxcobby <solomonsefah13@gmail.com>
2023-07-27 21:02:17 +0000

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*printing_main - entry point
+*Return: nothing
+*/
+
+void _attribute_((constructor)) printing_main()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
