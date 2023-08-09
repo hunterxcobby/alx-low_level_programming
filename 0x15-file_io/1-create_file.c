722ea16105cc88a99a88eac5bc65612cd3ffba9a
hunterxcobby <solomonsefah13@gmail.com>
2023-08-09 18:39:46 +0000

    final commit then i just give up and move to the next task because,"damn checker, my code works"

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,26 +12,22 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int letters;
-	/*char *BUFFER = NULL;*/
-	/*ssize_t read_data;*/ 
 	int written;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
-	
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-	{
-		perror("fails");
-		return (-1);
-	}
 
-	if(text_content == NULL)
+		return (-1);
+
+	if (text_content == NULL)
 		text_content = "";
 
 	for (letters = 0; text_content[letters]; letters++)
 
-	written = write(fd, text_content, letters);
+		written = write(fd, text_content, letters);
 	if (written == -1)
 		return (-1);
 
