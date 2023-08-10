4a169792efbc240ffa75975a26bb5ee8b3dafa6e
hunterxcobby <solomonsefah13@gmail.com>
2023-08-10 16:06:56 +0000

    fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,16 +24,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		text_content = "";
-	
+
 	for (letters = 0; text_content[letters] != '\0'; letters++)
 		;
 
 	written = write(fd, text_content, letters);
+
 	if (written == -1)
-	{
-		 close(fd);
+		close(fd);
 		return (-1);
-	}
 
 	close(fd);
 	return (1);
