5307d40dc3b8c64eaaf22586df62fadfe1532a68
hunterxcobby <solomonsefah13@gmail.com>
2023-08-10 16:08:58 +0000

    fixed compilation error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,8 +31,10 @@ int create_file(const char *filename, char *text_content)
 	written = write(fd, text_content, letters);
 
 	if (written == -1)
+	{
 		close(fd);
 		return (-1);
+	}
 
 	close(fd);
 	return (1);
