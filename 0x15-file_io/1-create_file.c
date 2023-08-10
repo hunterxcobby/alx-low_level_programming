2cdfb22b035b7cd141b7fccd9858b08d8142ce55
hunterxcobby <solomonsefah13@gmail.com>
2023-08-10 16:01:12 +0000

    removed the written operation from the loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,21 +18,24 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
 
+	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
+	
+	for (letters = 0; text_content[letters] != '\0'; letters++)
+		;
 
-	for (letters = 0; text_content[letters]; letters++)
-
-		written = write(fd, text_content, letters);
+	written = write(fd, text_content, letters);
 	if (written == -1)
+	{
+		 close(fd);
 		return (-1);
+	}
 
 	close(fd);
-
 	return (1);
 }
 
